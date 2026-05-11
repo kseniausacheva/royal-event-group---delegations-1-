1a6c4490808cd63dea5c7a03054190ee54a53206
kseniausacheva <arabsky153@gmail.com>
2026-05-11 15:49:10 +0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -40,7 +40,7 @@ const Contact = () => {
                 </div>
                 <div>
                   <h4 className="text-xl font-display font-bold uppercase mb-2">{t.contactPage.labels.email}</h4>
-                  <p className="text-white/40 text-lg">info@royalevent.group</p>
+                  <p className="text-white/40 text-lg">baxgat@yandex.ru</p>
                 </div>
               </div>
               <div className="flex items-start gap-8">
